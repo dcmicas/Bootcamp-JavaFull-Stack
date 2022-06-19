9dd3c5955000c6ca7c9591ca2cb749c88d9674ec
dcmicas <dcmicas@gmail.com>
2022-06-19 16:23:16 -0300

    Angular
    ToDo List

---
+++ Angular/ToDo_List/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  contador: number = 0;
+  simbolos: string [] = ['+', '-']
+
+  incrementar():void {
+    this.contador++
+  }
+
+  decrementar(): void{
+     this.contador--
+  }
+
+  adicionarValor(evento:any): void{
+    if (evento.target.value.length ==0) {
+      this.contador=0
+  } else {
+    this.contador = parseInt(evento.target.value)
+  }
+   
+  }
+}
